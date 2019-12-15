95724cf418ab3ab902b44cca78587e1ef875a946
joaorobson <joaorobsonmartins@gmail.com>
2019-12-14 22:45:03 -0300

    Add Makefile and format files using black

--- gpam_training/__init__.py
+++ gpam_training/__init__.py
@@ -2,6 +2,6 @@
 Module to facilitate the integration of a sklearn training pipeline into a deploy and retraining system
 """
 
-from .multilabel_training import MultilabelTraining 
+from .multilabel_training import MultilabelTraining
 
 __version__ = "0.0.1"
--- gpam_training/dataframe_preprocessing.py
+++ gpam_training/dataframe_preprocessing.py
@@ -2,15 +2,51 @@ from sklearn.preprocessing import MultiLabelBinarizer
 import numpy as np
 import pandas as pd
 
+
 class DataframePreprocessing:
-    
-    DEFAULT_TARGET_THEMES = [0, 5, 6, 26, 33, 139, 163, 232, 313, 339, 
-                             350, 406, 409, 555, 589, 597, 634, 660, 695, 
-                             729, 766, 773, 793, 800, 810, 852, 895, 951, 975]
+
+    DEFAULT_TARGET_THEMES = [
+        0,
+        5,
+        6,
+        26,
+        33,
+        139,
+        163,
+        232,
+        313,
+        339,
+        350,
+        406,
+        409,
+        555,
+        589,
+        597,
+        634,
+        660,
+        695,
+        729,
+        766,
+        773,
+        793,
+        800,
+        810,
+        852,
+        895,
+        951,
+        975,
+    ]
 
     OTHER_THEMES_VALUE = 4242
 
-    def __init__(self, df, x_column_name='page_text_extract', y_column_name='tema', target_themes=DEFAULT_TARGET_THEMES, other_themes_value=OTHER_THEMES_VALUE):
+    def __init__(
+        self,
+        df,
+        x_column_name="page_text_extract",
+        y_column_name="tema",
+        target_themes=DEFAULT_TARGET_THEMES,
+        other_themes_value=OTHER_THEMES_VALUE,
+    ):
         self.x_column_name = x_column_name
         self.y_column_name = y_column_name
         self.other_themes_value = other_themes_value
@@ -25,22 +61,26 @@ class DataframePreprocessing:
         self.processed_df = self._process_dataframe()
 
     def _group_samples_by_process(self, df):
-        print('Grouping processes...')
-        self.df = df.groupby('process_id').apply(self._aggregate)
+        print("Grouping processes...")
+        self.df = df.groupby("process_id").apply(self._aggregate)
 
     def _aggregate(self, series):
         reduced = {}
         series[self.x_column_name].drop_duplicates(inplace=True)
-       
-        reduced[self.x_column_name] = ' '.join(str(x) for x in series[self.x_column_name].values)
+
+        reduced[self.x_column_name] = " ".join(
+            str(x) for x in series[self.x_column_name].values
+        )
         temas = np.unique(series[self.y_column_name].values)
         reduced[self.y_column_name] = temas[~np.isnan(temas)]
-        return pd.Series(reduced, index=[self.x_column_name, *[self.y_column_name]])
+        return pd.Series(
+            reduced, index=[self.x_column_name, *[self.y_column_name]]
+        )
 
     def _remove_rare_samples(self, series, threshold=2):
         if self.labels_freq.get(tuple(series.tolist())) < threshold:
             return False
-        return True 
+        return True
 
     def _switch_other_themes_values(self, actual_label):
         """
@@ -58,9 +98,9 @@ class DataframePreprocessing:
 
     def _normalize_labels(self, series):
         return np.asarray(self._switch_other_themes_values(series.tolist()))
-    
+
     def _set_labels_frequency(self):
-        print('Setting labels frequency...')
+        print("Setting labels frequency...")
         for label in self.df[self.y_column_name]:
             normalized_label = tuple(self._switch_other_themes_values(label))
 
@@ -71,25 +111,36 @@ class DataframePreprocessing:
 
     def _get_distinct_themes(self):
         distinct_themes = set()
-        for label in self.df['labels_with_others']:
+        for label in self.df["labels_with_others"]:
             for theme in label:
                 distinct_themes.add(theme)
         self.distinct_themes = list(sorted(distinct_themes))
 
     def _binarize_labels(self):
-        print('Binarizing labels...')
+        print("Binarizing labels...")
         mlb = MultiLabelBinarizer()
-        binarized_columns = mlb.fit_transform(self.df['labels_with_others'].to_numpy())
-        
+        binarized_columns = mlb.fit_transform(
+            self.df["labels_with_others"].to_numpy()
+        )
+
         self._get_distinct_themes()
-        columns_names = {ix:binarized_columns[:,i] for i, ix in enumerate(self.distinct_themes)}
+        columns_names = {
+            ix: binarized_columns[:, i]
+            for i, ix in enumerate(self.distinct_themes)
+        }
 
-        return pd.concat([self.df.reset_index(drop=True), pd.DataFrame(columns_names)], axis=1) 
-
+        return pd.concat(
+            [self.df.reset_index(drop=True), pd.DataFrame(columns_names)],
+            axis=1,
+        )
 
     def _process_dataframe(self):
-        self.df['labels_with_others'] = self.df[self.y_column_name].apply(self._normalize_labels)
-        print('Removing rare samples...')
+        self.df["labels_with_others"] = self.df[self.y_column_name].apply(
+            self._normalize_labels
+        )
+        print("Removing rare samples...")
 
-        self.df = self.df[self.df['labels_with_others'].apply(self._remove_rare_samples)]
+        self.df = self.df[
+            self.df["labels_with_others"].apply(self._remove_rare_samples)
+        ]
         return self._binarize_labels()
