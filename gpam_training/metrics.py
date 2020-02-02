110c3c18e38134ca2a86846f89b21e76b7ce6a13
joaorobson <joaorobsonmartins@gmail.com>
2020-02-01 22:06:08 -0300

    Format files code style

--- gpam_training/metrics.py
+++ gpam_training/metrics.py
@@ -1,7 +1,13 @@
 import numpy as np
-from sklearn.metrics import classification_report, accuracy_score, \
-                            f1_score, precision_score, recall_score, hamming_loss, \
-                            jaccard_score
+from sklearn.metrics import (
+    classification_report,
+    accuracy_score,
+    f1_score,
+    precision_score,
+    recall_score,
+    hamming_loss,
+    jaccard_score,
+)
 
 
 def hit_one(y_true, y_pred):
@@ -14,6 +20,7 @@ def hit_one(y_true, y_pred):
             score.append(0)
     return sum(score) / len(score)
 
+
 def mistake_one(y_true, y_pred):
     score = []
     for pred, true in zip(y_pred, y_true):
@@ -24,6 +31,7 @@ def mistake_one(y_true, y_pred):
             score.append(0)
     return sum(score) / len(score)
 
+
 def true_positive(y_true, y_pred):
     score = []
     for pred, true in zip(y_pred, y_true):
@@ -32,7 +40,8 @@ def true_positive(y_true, y_pred):
         number_of_positives = len(true[indexes])
         if number_of_positives:
             score.append(correct / len(true[indexes]))
-    return sum(score)/ len(score)
+    return sum(score) / len(score)
+
 
 def true_negative(y_true, y_pred):
     score = []
@@ -42,7 +51,8 @@ def true_negative(y_true, y_pred):
         number_of_positives = len(true[indexes])
         if number_of_positives:
             score.append(correct / len(true[indexes]))
-    return sum(score)/ len(score)
+    return sum(score) / len(score)
+
 
 def false_positive(y_true, y_pred):
     score = []
@@ -56,6 +66,7 @@ def false_positive(y_true, y_pred):
             score.append(0)
     return sum(score) / len(score)
 
+
 def false_negative(y_true, y_pred):
     score = []
     for pred, true in zip(y_pred, y_true):
@@ -68,22 +79,36 @@ def false_negative(y_true, y_pred):
             score.append(0)
     return sum(score) / len(score)
 
-def get_multilabel_metrics(y_test, y_pred_pac):
-    one_hit = hit_one(y_test.values,y_pred_pac)
-    one_mistake = mistake_one(y_test.values, y_pred_pac)
-    tp = true_positive(y_test.values, y_pred_pac)
-    fp = false_positive(y_test.values, y_pred_pac)
-    tn = true_negative(y_test.values, y_pred_pac)
-    fn = false_negative(y_test.values, y_pred_pac)
-    hm = hamming_loss(y_test, y_pred_pac)
-    print('Métricas para Passive Agressive Classifier:\n')
+
+def get_binary_classification_metrics(y_test, y_pred):
+    acc = accuracy_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+    return {"acc": acc, "f1": f1}
+
+
+def get_multilabel_metrics(y_test, y_pred):
+    one_hit = hit_one(y_test.values, y_pred)
+    one_mistake = mistake_one(y_test.values, y_pred)
+    tp = true_positive(y_test.values, y_pred)
+    fp = false_positive(y_test.values, y_pred)
+    tn = true_negative(y_test.values, y_pred)
+    fn = false_negative(y_test.values, y_pred)
+    hm = hamming_loss(y_test, y_pred)
+    print("Métricas para XGBoost:\n")
     print("Pelo menos uma verdadeiro positivo: {}".format(one_hit))
     print("Pelo menos um falso positivo: {}".format(one_mistake))
     print("True positives: {}".format(tp))
     print("False positives: {}".format(fp))
     print("True negatives: {}".format(tn))
     print("False negatives: {}".format(fn))
-    print('Hamming loss:', hm)
+    print("Hamming loss:", hm)
 
-
-    return {'One hit': one_hit, 'One mistake': one_mistake, 'TP': tp, 'FP': fp, 'TN':tn, 'FN':fn, 'HL': hm}
+    return {
+        "One hit": one_hit,
+        "One mistake": one_mistake,
+        "TP": tp,
+        "FP": fp,
+        "TN": tn,
+        "FN": fn,
+        "HL": hm,
+    }
